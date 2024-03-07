a5c15857523a5e3743ca90657aaa12c86d03d255
Everton Mendonça Jesus <evertonmj.everdev@gmail.com>
2024-03-07 11:42:32 -0300

    First commit

---
+++ lpa/src/lpa_01/atividade_01/AtividadeDivisao.java
@@ -0,0 +1,14 @@
+package lpa_01.atividade_01;
+
+import java.util.Scanner;
+
+/*
+Missao: Faca a divisao de dois numeros e apresente o resultado em tela
+Caso o divisor seja zero, informe ao usuario que a divisao nao pode ser feita
+Por ser uma divisao de numeros inteiros considere que o resultado sera arredondado
+ */
+public class AtividadeDivisao {
+    public static void main(String args[]) {
+
+    }
+}
