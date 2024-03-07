3bb3089825064269fc570cc2817476b3a944e0e5
Everton Mendonça Jesus <evertonmj.everdev@gmail.com>
2024-03-07 11:33:00 -0300

    First commit

--- lpa/src/lpa_01/atividade_02/Atividade.java
+++ lpa/src/lpa_01/atividade_02/Atividade.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 /**
  * Missao: Imprimir a discografia do Led Zeppelin na tela
+ * O usuario devera informar o nome da banda como led
+ * Caso outro valor seja informado, o programa devera informar ao usuario
+ * que ele nao inseriu o nome da banda
+ * Utilize uma estrutura de repeticao para imprimir o nome dos quatro primeiros discos
  */
 //Classe eh o elemento principal de um programa java e SEMPRE deve iniciar com letra maiuscula
 public class Atividade {
@@ -20,10 +24,14 @@ public class Atividade {
 
         /*
             Discografia do Led Zeppelin
-            Led Zeppelin I
-            Led Zeppelin II
-            Led Zeppelin III
-            Led Zeppelin IV
+            Os quatro primeiros discos sao nomeados seguindo uma sequencia numerica
+            Utilize um loop para mostra-los e escrever menos codigo :)
+            Para facilitar a vida de voces, podem imprimir com numeros 1, 2, 3 e 4
+            Quem imprimir utilizando algarismos romanos ganha pontuacao extra
+                Led Zeppelin I
+                Led Zeppelin II
+                Led Zeppelin III
+                Led Zeppelin IV
             Houses of The Holly
             Physical Grafitti
             Presence
