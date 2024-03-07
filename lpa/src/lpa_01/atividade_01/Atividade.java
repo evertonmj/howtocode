1903b61b6ddd347c07f3428367f50514ab236444
Everton Mendonça Jesus <evertonmj.everdev@gmail.com>
2024-03-07 11:20:43 -0300

    First commit

---
+++ lpa/src/lpa_01/atividade_01/Atividade.java
@@ -0,0 +1,24 @@
+package lpa_01.atividade_01;
+
+import java.util.Scanner;
+
+/* Missao:
+    Receber dois numeros no teclado e imprimir sua soma
+ */
+
+//declaracao de uma classe java
+//todo nome de classe deve comecar com uma letra maiuscula
+public class Atividade {
+
+    //o metodo main eh o metodo incial do programa java
+    public static void main(String args[]) {
+        //a classe Scanner habilita a entrada de dados no programa
+        Scanner input = new Scanner(System.in);
+
+        //o metodo nextInt() espera que o usuario insira um numero inteiro no teclado
+        int primeiroNumero = input.nextInt();
+
+        //desenvolva seu codigo abaixo
+
+    }
+}
---
+++ lpa/src/lpa_01/atividade_02/Atividade.java
@@ -0,0 +1,33 @@
+package lpa_01.atividade_02;
+
+import java.util.Scanner;
+
+
+public class Atividade {
+    //Main eh o método prin
+    public static void main (String args[]) {
+        //Scanner eh uma classe utilizada para habilitar entrada de dados no programa
+        Scanner in = new Scanner(System.in);
+
+        //Leitura de dados inseridos
+        String banda = in.next();
+
+        //Impressao em tela
+        System.out.println("Iniciando o programa\n");
+
+        /*
+            Discografia do Led Zeppelin
+            Led Zeppelin I
+            Led Zeppelin II
+            Led Zeppelin III
+            Led Zeppelin IV
+            Houses of The Holly
+            Physical Grafitti
+            Presence
+            In Through The Out Door
+            Coda
+        */
+
+        System.out.println("\nFim do programa");
+    }
+}
